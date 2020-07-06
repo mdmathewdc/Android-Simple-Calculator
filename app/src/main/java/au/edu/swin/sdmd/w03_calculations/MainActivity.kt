4381a83969705dba770169411f9be29d81f141a0
N. A. Ronald <nronald@swin.edu.au>
2020-07-06 20:09:21 +1000

    First commit.

---
+++ app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
@@ -0,0 +1,27 @@
+package au.edu.swin.sdmd.w03_calculations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val number1 = findViewById<EditText>(R.id.number1)
+        val number2 = findViewById<EditText>(R.id.number2)
+
+        val equals = findViewById<Button>(R.id.equals)
+        equals.setOnClickListener { _ ->
+            val result = add(number1.text.toString(), number2.text.toString())
+            // TODO: show result on the screen
+        }
+    }
+
+    // adds two numbers together
+    private fun add(number1: String, number2: String) = number1 + number2
+
+}
