c620404ecb78120debff5c72e62810041da6312c
mdmathewdc <mathewdony007@gmail.com>
2020-08-12 22:38:13 +1000

    First commit

--- app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
@@ -1,10 +1,9 @@
 package au.edu.swin.sdmd.w03_calculations
 
+import android.app.PendingIntent.getActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,17 +13,36 @@ class MainActivity : AppCompatActivity() {
         val number1 = findViewById<EditText>(R.id.number1)
         val number2 = findViewById<EditText>(R.id.number2)
 
+        val plus = findViewById<RadioButton>(R.id.plus)
+        val minus = findViewById<RadioButton>(R.id.minus)
+        val result
 
         val equals = findViewById<Button>(R.id.equals)
         val answer = findViewById<TextView>(R.id.answer)
         equals.setOnClickListener { _ ->
-            val result = add(number1.text.toString(), number2.text.toString())
+            if (plus.isChecked) {
+                result = add(number1.text.toString(), number2.text.toString())
+            }
+            else
+            {
+                result = subtract(number1.text.toString(), number2.text.toString())
+            }
             // TODO: show result on the
             answer.text = result.toString()
         }
+
+        plus.setOnClickListener {
+            Toast.makeText(this@MainActivity, "PLUS clicked!", Toast.LENGTH_SHORT).show();
+        }
+
+        minus.setOnClickListener {
+            Toast.makeText(this@MainActivity, "MINUS clicked!", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     // adds two numbers together
     private fun add(number1: String, number2: String) = number1.toInt() + number2.toInt()
+    private fun subtract(number1: String, number2: String) = number1.toInt() - number2.toInt()
 
 }
